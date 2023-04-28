e66afcca6d437e0748add1903f55b54eedaf96a9
jimsorensen <au570491@uni.au.dk>
2023-04-28 10:22:17 +0200

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -2,40 +2,46 @@ using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
-// Video 51
-// https://github.com/TryCatchLearn/reactivities
-
-builder.Services.AddControllers();
-builder.Services.AddApplicationServices(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        // Video 53
+        // https://github.com/TryCatchLearn/reactivities
+
+        builder.Services.AddControllers();
+        builder.Services.AddApplicationServices(builder.Configuration);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors("CorsPolicy");
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedData(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error accored during migration");
+        }
+
+        app.Run();
+    }
 }
-
-app.UseCors("CorsPolicy");
-app.UseAuthorization();
-
-app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
-}
-catch (Exception ex)
-{    
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error accored during migration");
-}
-
-app.Run();
