d35ecca9987abbb6ff48743419e98d068ccafe16
jimsorensen <au570491@uni.au.dk>
2023-05-05 15:30:02 +0200

    End og section 12

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,8 +15,13 @@ internal class Program
         // Video 68
         // https://github.com/TryCatchLearn/reactivities
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(opt => 
+        {
+            var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            opt.Filters.Add(new AuthorizeFilter(policy));
+        });
         builder.Services.AddApplicationServices(builder.Configuration);
+        builder.Services.AddIdentityServices(builder.Configuration);
 
         var app = builder.Build();
 
@@ -25,6 +34,8 @@ internal class Program
         }
 
         app.UseCors("CorsPolicy");
+
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllers();
@@ -35,8 +46,9 @@ internal class Program
         try
         {
             var context = services.GetRequiredService<DataContext>();
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
             await context.Database.MigrateAsync();
-            await Seed.SeedData(context);
+            await Seed.SeedData(context, userManager);
         }
         catch (Exception ex)
         {
